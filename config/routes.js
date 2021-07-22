456fc6e5b7c03e13944c24f101df33ab33e56904
Glauber Oliveira <glauber17230@gmail.com>
2021-07-22 17:23:59 -0300

    Inserção de novas funções

--- config/routes.js
+++ config/routes.js
@@ -1,20 +1,35 @@
 const express = require('express')
 const routes = express.Router()
 
-let db=[{nome : 'abc', idade : '10'},
-{nome : 'abc1', idade : '12'},
-{nome : 'abc2', idade : '13'}]
+let produtos=[{nome : 'Arroz', validade : '4', preco: 7},
+{nome : 'Feijão', validade : '5', preco: 8},]
 
-routes.get('/',(req,res)=> {
-    return res.json(db)
+let clientes = [{nome:'Cicero Araújo', idade: 37, endereco:{ logradouro: 'Avenida Getúlio Vargas' , cidade: 'Feira de santana' , uf:'BA', numero: 1750}},
+{nome:'Amaro de Carvalho', idade: 55, endereco:{ logradouro: 'Avenida Tomé de Souza' , cidade: 'Feira de santana' , uf:'BA', numero: 200}}]
+
+routes.get('/produtos',(req,res)=> {
+    return res.json(produtos)
 })
 
-routes.post('/insert', (req,res) => {
+routes.post('/addProd', (req,res) => {
     const body = req.body
     
     if(!body)
         return res.status(400).end
-    db.push(body)
+    produtos.push(body)
+    return res.json(body)    
+})
+
+routes.get('/clientes',(req,res)=> {
+    return res.json(clientes)
+})
+
+routes.post('/addClin', (req,res) => {
+    const body = req.body
+    
+    if(!body)
+        return res.status(400).end
+    clientes.push(body)
     return res.json(body)    
 })
 
