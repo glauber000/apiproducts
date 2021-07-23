6e90a7bd49b820cc2075af5de377cea8895d9634
Glauber Oliveira <glauber17230@gmail.com>
2021-07-23 08:53:49 -0300

    inserção de novos campos no endereço

--- config/routes.js
+++ config/routes.js
@@ -4,8 +4,8 @@ const routes = express.Router()
 let produtos=[{nome : 'Arroz', validade : '4', preco: 7},
 {nome : 'Feijão', validade : '5', preco: 8},]
 
-let clientes = [{nome:'Cicero Araújo', idade: 37, endereco:{ logradouro: 'Avenida Getúlio Vargas' , cidade: 'Feira de santana' , uf:'BA', numero: 1750}},
-{nome:'Amaro de Carvalho', idade: 55, endereco:{ logradouro: 'Avenida Tomé de Souza' , cidade: 'Feira de santana' , uf:'BA', numero: 200}}]
+let clientes = [{nome:'Cicero Araújo', idade: 37, endereco:{ logradouro: 'Avenida Getúlio Vargas' , cidade: 'Feira de santana' , uf:'BA', numero: 1750, cep:'44075-425', bairro: 'Ponto Central'}},
+{nome:'Amaro de Carvalho', idade: 55, endereco:{ logradouro: 'Avenida Tomé de Souza' , cidade: 'Feira de santana' , uf:'BA', numero: 200, cep:'44023-432', bairro: 'Rua Nova'}}]
 
 routes.get('/produtos',(req,res)=> {
     return res.json(produtos)
